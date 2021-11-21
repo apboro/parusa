522f9d5c77e004d2647b84921d8c02aef63d5ab8
lv <lozovoyv@gmail.com>
2021-11-21 13:58:53 +0300

    Add migration subdirectories

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,5 +25,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        if ($this->app->runningInConsole()) {
+            $this->loadMigrationsFrom(database_path('migrations' . DIRECTORY_SEPARATOR . 'dictionaries'));
+            $this->loadMigrationsFrom(database_path('migrations' . DIRECTORY_SEPARATOR . 'user'));
+        }
     }
 }
