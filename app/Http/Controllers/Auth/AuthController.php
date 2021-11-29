296756dc84b2596ac2a190271096c0d82d0a4fd4
lv <lozovoyv@gmail.com>
2021-11-29 16:37:25 +0300

    Add auth controller and assets

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Response;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    /**
+     * Render login form.
+     *
+     * @return  View
+     */
+    public function form(): View
+    {
+        return view('login');
+    }
+
+    /**
+     * Handle an incoming authentication request.
+     *
+     * @param LoginRequest $request
+     *
+     * @return  JsonResponse
+     *
+     * @throws  ValidationException
+     */
+    public function login(LoginRequest $request): JsonResponse
+    {
+        $request->authenticate();
+
+        $request->session()->regenerate();
+
+        $intended = $request->session()->pull('url.intended', RouteServiceProvider::HOME);
+
+        return Response::response([
+            'redirect' => $intended,
+        ]);
+    }
+
+    /**
+     * Destroy an authenticated session.
+     *
+     * @param Request $request
+     *
+     * @return  JsonResponse
+     */
+    public function logout(Request $request): JsonResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return Response::response('OK');
+    }
+}
