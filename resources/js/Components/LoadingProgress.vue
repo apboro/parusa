7b38082cd4f7a4a7a401741bb1f01bde61730b8b
lv <lozovoyv@gmail.com>
2021-12-02 09:38:42 +0300

    Add loading progress component

---
+++ resources/js/Components/LoadingProgress.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="loading-progress__container">
+        <div v-if="loading" class="loading-progress__wrapper">
+            <div class="loading-progress"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+        </div>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        loading: {
+            type: Boolean,
+            default: false,
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
