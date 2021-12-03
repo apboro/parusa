cf5d9609ad225b7723353e17bfc03a695f5c2528
lv <lozovoyv@gmail.com>
2021-12-03 12:40:49 +0300

    add button and container components

---
+++ resources/js/Components/Base/BaseLinkButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <router-link :to="to" class="button" :class="classProxy">
+        <slot></slot>
+    </router-link>
+</template>
+
+<script>
+export default {
+    props: {
+        identifier: {type: String, default: null},
+        customClass: {type: String | Array | Object, default: null},
+        to: {type: Object, default: null},
+    },
+
+    computed: {
+        classProxy() {
+            return this.customClass;
+        }
+    },
+}
+</script>
---
+++ resources/js/Layouts/Parts/Container.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="page__container">
+        <slot/>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
