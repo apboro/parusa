8df7afe0a835925fa9dcb3babb7aea33b46e6490
lv <lozovoyv@gmail.com>
2021-12-05 10:28:35 +0300

    add birthday

--- app/Models/User/UserProfile.php
+++ app/Models/User/UserProfile.php
@@ -3,6 +3,7 @@
 namespace App\Models\User;
 
 use App\Models\Model;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
@@ -11,6 +12,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
  * @property string $firstname
  * @property string $patronymic
  * @property string $gender
+ * @property Carbon $birthdate
  *
  * @property User $user
  */
@@ -26,6 +28,11 @@ class UserProfile extends Model
 
     /** @var string[] Relations eager loading. */
     protected $with = ['user'];
+
+    /** @var string[] Attributes casting */
+    protected $casts = [
+        'birthdate' => 'date',
+    ];
 
     /**
      * User this profile belongs to.
