8b1a643b3b3228f47bdde7c149704d020ab1a2c5
lv <lozovoyv@gmail.com>
2021-12-17 19:40:37 +0300

    refactor

--- routes/api/sails.php
+++ routes/api/sails.php
@@ -4,10 +4,12 @@ use App\Http\Controllers\API\Sails\ExcursionCardController;
 use App\Http\Controllers\API\Sails\ExcursionDeleteController;
 use App\Http\Controllers\API\Sails\ExcursionEditController;
 use App\Http\Controllers\API\Sails\ExcursionsListController;
+use App\Http\Controllers\API\Sails\ExcursionStatusController;
 use App\Http\Controllers\API\Sails\PierCardController;
 use App\Http\Controllers\API\Sails\PierDeleteController;
 use App\Http\Controllers\API\Sails\PierEditController;
 use App\Http\Controllers\API\Sails\PiersListController;
+use App\Http\Controllers\API\Sails\PierStatusController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,13 +27,13 @@ Route::post('/piers', [PiersListController::class, 'list'])->middleware('auth:sa
 Route::post('/piers/view', [PierCardController::class, 'get'])->middleware('auth:sanctum');
 Route::post('/piers/get', [PierEditController::class, 'get'])->middleware('auth:sanctum');
 Route::post('/piers/update', [PierEditController::class, 'update'])->middleware('auth:sanctum');
-Route::post('/piers/status', [PierEditController::class, 'setStatus'])->middleware('auth:sanctum');
+Route::post('/piers/status', [PierStatusController::class, 'setStatus'])->middleware('auth:sanctum');
 Route::post('/piers/delete', [PierDeleteController::class, 'delete'])->middleware('auth:sanctum');
 
 Route::post('/excursions', [ExcursionsListController::class, 'list'])->middleware('auth:sanctum');
 Route::post('/excursions/view', [ExcursionCardController::class, 'get'])->middleware('auth:sanctum');
 Route::post('/excursions/get', [ExcursionEditController::class, 'get'])->middleware('auth:sanctum');
-Route::post('/excursions/status', [ExcursionEditController::class, 'setStatus'])->middleware('auth:sanctum');
 Route::post('/excursions/update', [ExcursionEditController::class, 'update'])->middleware('auth:sanctum');
+Route::post('/excursions/status', [ExcursionStatusController::class, 'setStatus'])->middleware('auth:sanctum');
 Route::post('/excursions/delete', [ExcursionDeleteController::class, 'delete'])->middleware('auth:sanctum');
 
