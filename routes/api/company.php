290d8b89f9a4dbafe937150647275d3690cb2cb7
lv <lozovoyv@gmail.com>
2021-12-21 16:11:27 +0300

    update staff routes

--- routes/api/company.php
+++ routes/api/company.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\API\Company\StaffAccessController;
 use App\Http\Controllers\API\Company\StaffCardController;
 use App\Http\Controllers\API\Company\StaffDeleteController;
 use App\Http\Controllers\API\Company\StaffEditController;
 use App\Http\Controllers\API\Company\StaffListController;
+use App\Http\Controllers\API\Company\StaffStatusController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,5 +23,8 @@ Route::post('/company/staff', [StaffListController::class, 'list'])->middleware(
 Route::post('/company/staff/view', [StaffCardController::class, 'get'])->middleware('auth:sanctum');
 Route::post('/company/staff/get', [StaffEditController::class, 'get'])->middleware('auth:sanctum');
 Route::post('/company/staff/update', [StaffEditController::class, 'update'])->middleware('auth:sanctum');
+Route::post('/company/staff/status', [StaffStatusController::class, 'setStatus'])->middleware('auth:sanctum');
 Route::post('/company/staff/delete', [StaffDeleteController::class, 'delete'])->middleware('auth:sanctum');
+Route::post('/company/staff/access/set', [StaffAccessController::class, 'set'])->middleware('auth:sanctum');
+Route::post('/company/staff/access/release', [StaffAccessController::class, 'release'])->middleware('auth:sanctum');
 
