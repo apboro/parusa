658935889e85053c8335a50a23b19e0de1163753
lv <lozovoyv@gmail.com>
2021-12-22 10:55:16 +0300

    add action cell to header option

--- resources/js/Components/Table/BaseTableHead.vue
+++ resources/js/Components/Table/BaseTableHead.vue
@@ -2,6 +2,7 @@
     <thead class="base-table__header" v-if="headers">
     <tr class="base-table__header-row">
         <td class="base-table__header-cell" v-for="(cell, key) in headers" :key="key">{{ cell }}</td>
+        <td class="base-table__header-cell base-table__header-cell-actions" v-if="hasActions"></td>
     </tr>
     </thead>
 </template>
@@ -11,10 +12,8 @@ import empty from "../../Helpers/Lib/empty";
 
 export default {
     props: {
-        header: {
-            type: [Array, Object],
-            default: null,
-        }
+        header: {type: [Array, Object], default: null},
+        hasActions: {type: Boolean, default: false},
     },
 
     computed: {
