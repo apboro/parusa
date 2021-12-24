792d7bd3f5dc4fa2c92a5b398d675a03dfc664e4
lv <lozovoyv@gmail.com>
2021-12-24 11:28:54 +0300

    add hint

---
+++ resources/js/Components/GUI/Hint.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="hint" :class="classFromAttributes">
+        <slot/>
+    </div>
+</template>
+
+<script>
+import AttributeKeysToClass from "../../Mixins/AttributeKeysToClass";
+
+export default {
+    inheritAttrs: false,
+    mixins: [AttributeKeysToClass],
+}
+</script>
