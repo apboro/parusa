f0f2e013db2fdf8b096b131b12eac313ad25966d
lv <lozovoyv@gmail.com>
2021-12-24 11:29:14 +0300

    add lock displaying

---
+++ resources/js/Components/ActivityLocked.vue
@@ -0,0 +1,19 @@
+<template>
+    <icon-lock :class="['activity-locked', 'activity-locked__locked']" v-if="locked"/>
+    <icon-unlock :class="['activity-locked', 'activity-locked__unlocked']" v-else/>
+</template>
+
+<script>
+import IconLock from "./Icons/IconLock";
+import IconUnlock from "./Icons/IconUnlock";
+
+export default {
+    components: {IconUnlock, IconLock},
+    props: {
+        locked: {
+            type: Boolean,
+            default: true
+        }
+    },
+}
+</script>
