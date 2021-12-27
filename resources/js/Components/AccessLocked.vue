cd2a7643abf7bfff296d5602888f1e91a0c68571
lv <lozovoyv@gmail.com>
2021-12-27 15:33:27 +0300

    update access locked

--- resources/js/Components/AccessLocked.vue
+++ resources/js/Components/AccessLocked.vue
@@ -1,14 +1,17 @@
 <template>
-    <icon-lock :class="['activity-locked', 'activity-locked__locked']" v-if="locked"/>
-    <icon-unlock :class="['activity-locked', 'activity-locked__unlocked']" v-else/>
+    <icon-ban :class="['activity-locked', 'activity-locked__locked']" v-if="locked"/>
+    <icon-arrow-right :class="['activity-locked', 'activity-locked__unlocked']" v-else/>
 </template>
 
 <script>
-import IconLock from "./Icons/IconLock";
-import IconUnlock from "./Icons/IconUnlock";
+import IconArrowRight from "./Icons/IconArrowRight";
+import IconBan from "./Icons/IconBan";
 
 export default {
-    components: {IconUnlock, IconLock},
+    components: {
+        IconBan,
+        IconArrowRight,
+    },
     props: {
         locked: {
             type: Boolean,
