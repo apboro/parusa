4a57f8b0e4710a8015f82b593d36a960ec8ef807
lv <lozovoyv@gmail.com>
2022-01-05 10:46:05 +0300

    add expand arrow element

---
+++ resources/js/Components/Expand.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="expand" :class="{'expand__expanded': expanded}" @click="expand">
+        <icon-dropdown :class="'expand__button'"/>
+    </div>
+</template>
+
+<script>
+import IconDropdown from "./Icons/IconDropdown";
+
+export default {
+    emits: ['expand'],
+
+    components: {
+        IconDropdown,
+    },
+
+    data: () => ({
+        expanded: false,
+    }),
+
+    methods: {
+        expand() {
+            this.expanded = !this.expanded;
+            this.$emit('expand', this.expanded);
+        }
+    }
+}
+</script>
