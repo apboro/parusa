30a63975e450f96f904b609df75ea437e38feb7b
lv <lozovoyv@gmail.com>
2022-02-04 21:34:41 +0300

    refactor

--- resources/js/Components/PopUp.vue
+++ resources/js/Components/PopUp.vue
@@ -1,43 +1,52 @@
 <template>
-    <div class="dialogs__overlay" v-if="shown"
-         :class="{
-            'dialogs__overlay-hide': hiding,
-            'dialogs__overlay-shown': showing,
-         }" @click="popupClose"
-    >
+    <div class="dialogs__overlay" v-if="shown" :class="{'dialogs__overlay-hide': hiding, 'dialogs__overlay-shown': showing}" @click="popupClose">
+
         <div class="dialogs__dialog">
-            <loading-progress :loading="processing" :opacity="70">
+            <LoadingProgress :loading="processing">
+
                 <div class="dialogs__dialog-wrapper">
-                    <div class="dialogs__dialog-message" v-if="title">
-                        <div class="dialogs__dialog-message-text">{{ title }}</div>
+
+                    <div class="dialogs__dialog-title" v-if="title">{{ title }}</div>
+
+                    <div class="dialogs__dialog-message" v-if="message">
+                        <div class="dialogs__dialog-message-text">{{ message }}</div>
                     </div>
+
                     <slot/>
+
                     <div class="dialogs__dialog-buttons" :class="'dialogs__dialog-buttons-' + align">
-                <span v-for="button in buttons"
-                      class="button"
-                      :class="' button__' + button.color"
-                      @click="resolve(button.result)"
-                >{{ button.caption }}</span>
+                        <GuiButton v-for="button in buttons"
+                                   :color="button.color"
+                                   :identifier="button.result"
+                                   @clicked="resolve"
+                        >
+                            {{ button.caption }}
+                        </GuiButton>
                     </div>
+
                 </div>
-            </loading-progress>
+
+            </LoadingProgress>
         </div>
+
     </div>
 </template>
 
 <script>
-import LoadingProgress from "./LoadingProgress";
+import LoadingProgress from "@/Components/LoadingProgress";
+import GuiButton from "@/Components/GUI/GuiButton";
 
 export default {
-    components: {LoadingProgress},
+    components: {GuiButton, LoadingProgress},
     props: {
+        title: {type: String, default: null},
+        message: {type: String, default: null},
         buttons: {
             type: Array,
             default: () => ([{result: 'ok', caption: 'OK', color: 'white'}])
         },
         align: {type: String, default: 'center'},
         manual: {type: Boolean, default: false},
-        title: {type: String, default: null},
         resolving: {type: Function, default: null},
         closeOnOverlay: {type: Boolean, default: false},
     },
@@ -98,3 +107,133 @@ export default {
     }
 }
 </script>
+
+<style lang="scss">
+
+$project_font: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji !default;
+$animation_time: 150ms !default;
+$animation: cubic-bezier(0.24, 0.19, 0.28, 1.29) !default;
+$shadow_2: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23) !default;
+$base_white_color: #ffffff !default;
+$base_black_color: #1e1e1e !default;
+$base_red_color: #EA1B00 !default;
+$base_green_color: #00AF2E !default;
+$base_primary_color: #0D74D7 !default;
+$base_orange_color: #EA8B00 !default;
+$base_light_gray_color: #e5e5e5 !default;
+
+.dialogs {
+    &__overlay {
+        position: fixed;
+        z-index: 300;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        backdrop-filter: blur(1px);
+        background-color: rgba(0, 0, 0, 0.2);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        opacity: 0;
+        visibility: hidden;
+        transition: opacity $animation $animation_time, visibility $animation $animation_time;
+
+        &-shown {
+            opacity: 1;
+            visibility: visible;
+        }
+
+        &-hide {
+            opacity: 0;
+            visibility: hidden;
+        }
+    }
+
+    &__dialog {
+        background-color: $base_white_color;
+        border-radius: 2px;
+        box-sizing: border-box;
+        padding: 15px;
+        box-shadow: $shadow_2;
+
+        &-wrapper {
+            box-sizing: border-box;
+            padding: 15px;
+            width: 100%;
+            height: 100%;
+        }
+
+        &-title {
+            display: block;
+            font-family: $project_font;
+            font-size: 16px;
+            font-weight: bold;
+            text-align: left;
+            color: $base_black_color;
+            margin-bottom: 20px;
+        }
+
+        &-message {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            margin-bottom: 20px;
+
+            &-icon {
+                width: 32px;
+                height: 32px;
+                margin-right: 30px;
+                color: $base_black_color;
+
+                &-red {
+                    color: $base_red_color;
+                }
+
+                &-green {
+                    color: $base_green_color;
+                }
+
+                &-blue {
+                    color: $base_primary_color;
+                }
+
+                &-orange {
+                    color: $base_orange_color;
+                }
+            }
+
+            &-text {
+                font-family: $project_font;
+                font-size: 14px;
+                color: $base_black_color;
+                text-align: left;
+                flex-grow: 1;
+            }
+        }
+
+        &-buttons {
+            margin-top: 30px;
+            box-sizing: border-box;
+            padding-top: 20px;
+            border-top: 1px solid $base_light_gray_color;
+
+            &-left {
+                text-align: left
+            }
+
+            &-center {
+                text-align: center
+            }
+
+            &-right {
+                text-align: right
+            }
+
+            & > * {
+                min-width: 100px;
+            }
+        }
+    }
+}
+</style>
