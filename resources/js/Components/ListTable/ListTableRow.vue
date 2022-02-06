315c53afc397b1a7cb7b8b37f43e6396ab865a4c
lv <lozovoyv@gmail.com>
2022-02-06 17:35:35 +0300

    add list table components

---
+++ resources/js/Components/ListTable/ListTableRow.vue
@@ -0,0 +1,34 @@
+<template>
+    <tr class="list-table__row">
+        <slot/>
+    </tr>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+@import "../variables";
+
+$list_table_border_color: #e3e3e3 !default;
+$list_table_hover_background_color: #f5faff !default;
+$list_table_odd_background_color: #ffffff !default;
+$list_table_even_background_color: #fbfbfb !default;
+
+.list-table__row {
+    border-bottom: 1px solid $list_table_border_color;
+
+    &:nth-child(odd) {
+        background-color: $list_table_odd_background_color;
+    }
+
+    &:nth-child(even) {
+        background-color: $list_table_even_background_color;
+    }
+
+    &:hover {
+        background-color: $list_table_hover_background_color;
+    }
+}
+</style>
