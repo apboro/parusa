a1852639a55266eeae66edeebcf00570484b0c74
lv <lozovoyv@gmail.com>
2022-02-14 20:28:00 +0300

    add fields v2

---
+++ resources/js/Components/Fields/FieldDropDown.vue
@@ -0,0 +1,81 @@
+<template>
+    <FieldWrapper :title="title" :required="required" :disabled="disabled" :valid="valid" :errors="errors">
+        <InputDropDown
+            v-model="proxyValue"
+            :name="name"
+            :original="original"
+            :valid="valid"
+            :disabled="disabled"
+            :has-null="hasNull"
+            :options="options"
+            :disabled-options="disabledOptions"
+            :identifier="identifier"
+            :show="show"
+            :search="search"
+            :top="top"
+            :right="right"
+            :center="center"
+            :placeholder="placeholder"
+            :small="small"
+            @change="change"
+            ref="input"
+        />
+    </FieldWrapper>
+</template>
+
+<script>
+import FieldWrapper from "@/Components/Fields/Helpers/FieldWrapper";
+import InputDropDown from "@/Components/Inputs/InputDropDown";
+
+export default {
+    components: {InputDropDown, FieldWrapper},
+    props: {
+        name: {type: String, required: true},
+        modelValue: {type: [Boolean, String, Number, Object], default: null},
+        original: {type: [Boolean, String, Number, Object], default: null},
+
+        title: {type: String, default: null},
+        required: {type: Boolean, default: false},
+        disabled: {type: Boolean, default: false},
+        valid: {type: Boolean, default: true},
+        errors: {type: Array, default: null},
+
+        placeholder: {type: String, default: null},
+        small: {type: Boolean, default: false},
+
+        options: {type: Array, default: () => ([])},
+        disabledOptions: {type: Boolean, default: false},
+        identifier: {type: String, default: null},
+        show: {type: String, default: null},
+        hasNull: {type: Boolean, default: false},
+
+        search: {type: Boolean, default: false},
+        top: {type: Boolean, default: false},
+        right: {type: Boolean, default: false},
+        center: {type: Boolean, default: false},
+    },
+
+    emits: ['update:modelValue', 'change'],
+
+    computed: {
+        proxyValue: {
+            get() {
+                return this.modelValue
+            },
+            set(value) {
+                this.$emit('update:modelValue', value);
+            }
+        }
+    },
+
+    methods: {
+        focus() {
+            this.$refs.input.focus()
+        },
+        change(value, name) {
+            this.$emit('change', value, name);
+        },
+    }
+}
+</script>
+
---
+++ resources/js/Components/Fields/Helpers/FieldWrapper.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="field" :class="{'field__required': required}">
+        <span class="field__title">{{ title }}</span>
+        <div class="field__wrapper">
+            <div class="field__input">
+                <slot/>
+            </div>
+            <div class="field__errors">
+                <span class="field__errors-error" v-if="!valid" v-for="error in errors">{{ error }}</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: {
+        title: String,
+        required: {type: Boolean, default: false},
+        disabled: {type: Boolean, default: false},
+        valid: {type: Boolean, default: true},
+        errors: {type: Array, default: () => ([])},
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>
