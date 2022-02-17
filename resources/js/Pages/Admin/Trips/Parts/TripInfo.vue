c2e16b612e1cecabc15b94b7ccee01aa551b09f7
lv <lozovoyv@gmail.com>
2022-02-17 20:46:19 +0300

    refactor popup forms

--- resources/js/Pages/Admin/Trips/Parts/TripInfo.vue
+++ resources/js/Pages/Admin/Trips/Parts/TripInfo.vue
@@ -47,29 +47,28 @@
             <GuiButton @click="edit">Редактировать</GuiButton>
         </GuiContainer>
 
-        <!--        <pop-up ref="status_popup" :title="popup_title"-->
-        <!--                :buttons="[{result: 'no', caption: 'Отмена', color: 'white'}, {result: 'yes', caption: 'OK', color: 'green'}]"-->
-        <!--                :manual="true"-->
-        <!--        >-->
-        <!--            <dictionary-drop-down :dictionary="popup_dictionary" v-model="current_status" :name="'status'" :original="initial_status"/>-->
-        <!--        </pop-up>-->
-
-        <!--        <pop-up ref="form_popup" v-if="editable" :title="form_popup_title"-->
-        <!--                :buttons="[{result: 'no', caption: 'Отмена', color: 'white'}, {result: 'yes', caption: 'OK', color: 'green'}]"-->
-        <!--                :manual="true"-->
-        <!--        >-->
-        <!--            <data-field-input :datasource="form" :name="'input'" :type="'number'"/>-->
-        <!--        </pop-up>-->
-
+        <FormPopUp :title="form_title"
+                   :form="form"
+                   :options="{id: tripId}"
+                   ref="popup"
+        >
+            <GuiContainer w-350px>
+                <FormDictionary v-if="dictionary !== null" :form="form" :name="'value'" :dictionary="dictionary" :hide-title="true"/>
+                <FormNumber v-else :form="form" :name="'value'" :hide-title="true"/>
+            </GuiContainer>
+        </FormPopUp>
     </div>
 </template>
 
 <script>
-
+import form from "@/Core/Form";
 import GuiContainer from "@/Components/GUI/GuiContainer";
 import GuiValue from "@/Components/GUI/GuiValue";
 import GuiHint from "@/Components/GUI/GuiHint";
 import GuiButton from "@/Components/GUI/GuiButton";
+import FormPopUp from "@/Components/FormPopUp";
+import FormDictionary from "@/Components/Form/FormDictionary";
+import FormNumber from "@/Components/Form/FormNumber";
 
 export default {
     props: {
@@ -79,154 +78,69 @@ export default {
     },
 
     components: {
+        FormNumber,
+        FormDictionary,
+        FormPopUp,
         GuiButton,
         GuiHint,
         GuiValue,
-        GuiContainer
-
+        GuiContainer,
     },
 
     data: () => ({
-        // popup_title: null,
-        // popup_dictionary: null,
-        // initial_status: null,
-        // current_status: null,
-        // form: null,
-        // form_popup_title: null,
+        form: form('', '/api/trips/properties'),
+        form_title: null,
+        dictionary: null,
     }),
 
     methods: {
         edit() {
             this.$router.push({name: 'trip-edit', params: {id: this.tripId}});
         },
+        initForm(title, key, dictionary = null) {
+            this.form_title = title;
+            this.form.reset();
+            this.form.set('name', key);
+            this.form.set('value', this.data[key], 'required', title, true);
+            this.dictionary = dictionary;
+            this.form.toaster = this.$toast;
+            this.form.load();
+        },
         statusChange() {
+            this.initForm('Статус движения', 'status_id', 'trip_statuses');
+            this.$refs.popup.show()
+                .then(response => {
+                    console.log(response);
+                })
         },
         saleStatusChange() {
+            this.initForm('Статус продаж', 'sale_status_id', 'trip_sale_statuses');
+            this.$refs.popup.show()
+                .then(response => {
+                    console.log(response);
+                })
         },
         ticketsCountChange() {
+            this.initForm('Общее количество билетов', 'tickets_total');
+            this.$refs.popup.show()
+                .then(response => {
+                    console.log(response);
+                })
         },
         discountStatusChange() {
+            this.initForm('Скидки от базовой цены на кассах', 'discount_status_id', 'trip_statuses');
+            this.$refs.popup.show()
+                .then(response => {
+                    console.log(response);
+                })
         },
         cancellationTimeChange() {
+            this.initForm('Время аннулирования брони, за Х мин.', 'discount_status_id');
+            this.$refs.popup.show()
+                .then(response => {
+                    console.log(response);
+                })
         },
-        // statusChange() {
-        //     this.popup_title = 'Статус движения';
-        //     this.popup_dictionary = 'trip_statuses';
-        //     this.initial_status = Number(this.datasource.data['status_id']);
-        //     this.current_status = this.initial_status;
-        //     this.genericStatusChange('/api/trips/status')
-        //         .then(data => {
-        //             this.datasource.data['status'] = data['status'];
-        //             this.datasource.data['status_id'] = data['status_id'];
-        //         });
-        // },
-        //
-        // saleStatusChange() {
-        //     this.popup_title = 'Статус продаж';
-        //     this.popup_dictionary = 'trip_sale_statuses';
-        //     this.initial_status = Number(this.datasource.data['sale_status_id']);
-        //     this.current_status = this.initial_status;
-        //     this.genericStatusChange('/api/trips/sale-status')
-        //         .then(data => {
-        //             this.datasource.data['sale_status'] = data['status'];
-        //             this.datasource.data['sale_status_id'] = data['status_id'];
-        //         });
-        // },
-        //
-        // discountStatusChange() {
-        //     this.popup_title = 'Скидки от базовой цены';
-        //     this.popup_dictionary = 'trip_discount_statuses';
-        //     this.initial_status = Number(this.datasource.data['discount_status_id']);
-        //     this.current_status = this.initial_status;
-        //     this.genericStatusChange('/api/trips/discount-status')
-        //         .then(data => {
-        //             this.datasource.data['discount_status'] = data['status'];
-        //             this.datasource.data['discount_status_id'] = data['status_id'];
-        //         });
-        // },
-        //
-        // ticketsAfterSave(payload) {
-        //     this.datasource.data['tickets_total'] = payload['tickets_total'];
-        //     this.form.loaded = false;
-        //     this.$refs.form_popup.hide();
-        // },
-        //
-        // timeAfterSave(payload) {
-        //     this.datasource.data['cancellation_time'] = payload['cancellation_time'];
-        //     this.form.loaded = false;
-        //     this.$refs.form_popup.hide();
-        // },
-        //
-        // ticketsCountChange() {
-        //     this.form_popup_title = 'Общее количество билетов';
-        //     this.form = formDataSource(null, '/api/trips/tickets-count', {id: this.tripId});
-        //     const input = Number(this.datasource.data['tickets_total']);
-        //     this.form.titles = {input: 'Общее количество билетов'};
-        //     this.form.values = {input: input};
-        //     this.form.originals = {input: input};
-        //     this.form.validation_rules = {input: parseRules('required|integer|min:1|bail')};
-        //     this.form.afterSave = this.ticketsAfterSave;
-        //     this.form.toaster = this.$toast;
-        //     this.form.loaded = true;
-        //
-        //     this.$refs.form_popup.show()
-        //         .then(result => {
-        //             if (result === 'yes') {
-        //                 this.$refs.form_popup.process(true);
-        //                 this.form.save();
-        //             } else {
-        //                 this.$refs.form_popup.hide();
-        //             }
-        //         });
-        // },
-        //
-        // cancellationTimeChange() {
-        //     this.form_popup_title = 'Время аннулирования брони';
-        //     this.form = formDataSource(null, '/api/trips/cancellation-time', {id: this.tripId});
-        //     const input = Number(this.datasource.data['cancellation_time']);
-        //     this.form.titles = {input: 'Время аннулирования брони на рейс, мин.'};
-        //     this.form.values = {input: input};
-        //     this.form.originals = {input: input};
-        //     this.form.validation_rules = {input: parseRules('required|integer|min:0|bail')};
-        //     this.form.afterSave = this.timeAfterSave;
-        //     this.form.toaster = this.$toast;
-        //     this.form.loaded = true;
-        //
-        //     this.$refs.form_popup.show()
-        //         .then(result => {
-        //             if (result === 'yes') {
-        //                 this.$refs.form_popup.process(true);
-        //                 this.form.save();
-        //             } else {
-        //                 this.$refs.form_popup.hide();
-        //             }
-        //         });
-        // },
-        //
-        // genericStatusChange(url) {
-        //     return new Promise((resolve, reject) => {
-        //         this.$refs.status_popup.show()
-        //             .then(result => {
-        //                 if (result === 'yes') {
-        //                     this.$refs.status_popup.process(true);
-        //                     axios.post(url, {id: this.tripId, status_id: this.current_status})
-        //                         .then(response => {
-        //                             this.$toast.success(response.data.message, 3000);
-        //                             resolve(response.data.data);
-        //                         })
-        //                         .catch(error => {
-        //                             this.$toast.error(error.response.data.message);
-        //                             reject();
-        //                         })
-        //                         .finally(() => {
-        //                             this.$refs.status_popup.hide();
-        //                         })
-        //                 } else {
-        //                     this.$refs.status_popup.hide();
-        //                 }
-        //             });
-        //     });
-        // }
     }
 }
 </script>
