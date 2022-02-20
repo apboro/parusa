e89388b39f420e87ba46bff21e29115fdac6de19
lv <lozovoyv@gmail.com>
2022-02-20 14:29:46 +0300

    add text field adn form field

---
+++ resources/js/Components/Form/FormText.vue
@@ -0,0 +1,33 @@
+<template>
+    <FieldText
+        :model-value="value"
+        :name="name"
+        :title="title"
+        :hide-title="hideTitle"
+        :original="original"
+        :valid="valid"
+        :errors="errors"
+        :required="required"
+        :disabled="disabled"
+        :placeholder="placeholder"
+        :small="small"
+        @change="change"
+        ref="input"
+    />
+</template>
+
+<script>
+import FormMixin from "@/Components/Form/Helpers/FormMixin";
+import FieldText from "@/Components/Fields/FieldText";
+
+export default {
+    components: {FieldText},
+    props: {
+        disabled: {type: Boolean, default: false},
+        placeholder: {type: String, default: null},
+        small: {type: Boolean, default: false},
+    },
+
+    mixins: [FormMixin],
+}
+</script>
