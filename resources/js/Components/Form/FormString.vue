c8e583ec45de3beb8e3d391923a7a125b55f5e53
lv <lozovoyv@gmail.com>
2022-02-20 13:57:44 +0300

    add form string component

---
+++ resources/js/Components/Form/FormString.vue
@@ -0,0 +1,37 @@
+<template>
+    <FieldString
+        :model-value="value"
+        :name="name"
+        :title="title"
+        :hide-title="hideTitle"
+        :original="original"
+        :valid="valid"
+        :errors="errors"
+        :required="required"
+        :disabled="disabled"
+        :placeholder="placeholder"
+        :type="type"
+        :autocomplete="autocomplete"
+        :small="small"
+        @change="change"
+        ref="input"
+    />
+</template>
+
+<script>
+import FormMixin from "@/Components/Form/Helpers/FormMixin";
+import FieldString from "@/Components/Fields/FieldString";
+
+export default {
+    components: {FieldString},
+    props: {
+        disabled: {type: Boolean, default: false},
+        placeholder: {type: String, default: null},
+        small: {type: Boolean, default: false},
+        type: {type: String, default: 'text'},
+        autocomplete: {type: String, default: 'off'},
+    },
+
+    mixins: [FormMixin],
+}
+</script>
