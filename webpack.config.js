0a292a9d5c6798dfd7ccd3a082b1cb7df54defb4
lv <lozovoyv@gmail.com>
2022-03-30 18:14:42 +0300

    update apps

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ module.exports = {
     resolve: {
         alias: {
             '@': path.resolve('resources/js'),
+            config: path.join(__dirname, 'resources/js/config', process.env.NODE_ENV)
         },
     },
 };
