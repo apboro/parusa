a69d6dedb7fe270c8f04a3425255faced8d0d6b3
lv <lozovoyv@gmail.com>
2022-05-31 14:29:28 +0300

    fix terminal users ids

--- app/Http/Controllers/API/Terminals/TerminalStaffController.php
+++ app/Http/Controllers/API/Terminals/TerminalStaffController.php
@@ -41,7 +41,7 @@ class TerminalStaffController extends ApiController
         $terminal->staff()->attach($position->id);
 
         return APIResponse::success('Кассир прикреплён', [
-            'id' => $position->id,
+            'id' => $position->user_id,
             'name' => $position->user->profile->fullName,
             'active' => $position->hasStatus(PositionStatus::active),
             'position' => $position->title,
