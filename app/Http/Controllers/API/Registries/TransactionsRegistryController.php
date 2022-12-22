2715d6c605100f938216e3021c552dc5c5002442
sansay500 <borodachev@gmail.com>
2022-12-22 16:00:31 +0300

    Экспорт реестров по кассам

--- app/Http/Controllers/API/Registries/TransactionsRegistryController.php
+++ app/Http/Controllers/API/Registries/TransactionsRegistryController.php
@@ -16,6 +16,8 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\Storage;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
 
 class TransactionsRegistryController extends ApiController
 {
@@ -32,6 +34,8 @@ class TransactionsRegistryController extends ApiController
 
     protected string $rememberKey = CookieKeys::transactions_registry_list;
 
+    public ?int $terminalId;
+
     /**
      * Get transactions list.
      *
@@ -41,30 +45,176 @@ class TransactionsRegistryController extends ApiController
      */
     public function list(ApiListRequest $request): JsonResponse
     {
-        $current = Currents::get($request);
-
         $this->defaultFilters['date_from'] = Carbon::now()->startOfDay()->format('Y-m-d\TH:i');
         $this->defaultFilters['date_to'] = Carbon::now()->endOfDay()->format('Y-m-d\TH:i');
         $filters = $request->filters($this->defaultFilters, $this->rememberFilters, $this->rememberKey);
 
-        $query = Payment::query()
-            ->with(['status', 'order', 'terminal', 'position', 'position.user', 'position.user.profile']);
-
         $dateFrom = Carbon::parse($filters['date_from'])->seconds(0)->microseconds(0);
         $dateTo = Carbon::parse($filters['date_to'])->seconds(59)->microseconds(999);
+
         $filters['date_from'] = $dateFrom->format('Y-m-d\TH:i');
         $filters['date_to'] = $dateTo->format('Y-m-d\TH:i');
 
-        $terminalId = null;
+        $this->terminalId = null;
+
+        $query = $this->getListQuery($request, $filters, Currents::get($request));
+
+        $queryBackup = $query->clone();
+        $payments = $query->paginate($request->perPage(10, $this->rememberKey));
+
+        /** @var LengthAwarePaginator $payments */
+        $payments->transform(function (Payment $payment) {
+            return [
+                'id' => $payment->id,
+                'gate' => $payment->gate,
+                'external_id' => $payment->external_id,
+                'date' => $payment->created_at->format('d.m.Y'),
+                'time' => $payment->created_at->format('H:i'),
+                'order_id' => $payment->order_id,
+                'order_external_id' => $payment->order->external_id ?? null,
+                'status' => $payment->status->name,
+                'status_id' => $payment->status_id,
+                'fiscal' => $payment->fiscal,
+                'total' => $payment->total,
+                'by_card' => $payment->by_card,
+                'by_cash' => $payment->by_cash,
+                'terminal' => $payment->terminal->name ?? null,
+                'terminal_id' => $payment->terminal_id,
+                'position' => $payment->position ? $payment->position->user->profile->compactName : null,
+                'position_id' => $payment->position->user_id ?? null,
+            ];
+        });
+
+        $saleQuery = $queryBackup->clone()->where('status_id', PaymentStatus::sale);
+        $returnQuery = $queryBackup->clone()->where('status_id', PaymentStatus::return);
+
+        $saleTotal = PriceConverter::storeToPrice($saleQuery->sum('total'));
+        $saleByCard = PriceConverter::storeToPrice($saleQuery->sum('by_card'));
+        $saleByCash = PriceConverter::storeToPrice($saleQuery->sum('by_cash'));
+
+        $returnTotal = PriceConverter::storeToPrice($returnQuery->sum('total'));
+        $returnByCard = PriceConverter::storeToPrice($returnQuery->sum('by_card'));
+        $returnByCash = PriceConverter::storeToPrice($returnQuery->sum('by_cash'));
+
+        $overallTotal = $saleTotal - $returnTotal;
+
+        return APIResponse::list(
+            $payments,
+            ['Дата и время', '№ заказа', 'Тип, чек', 'Сумма', 'Касса, кассир', 'Идентификаторы'],
+            $filters,
+            $this->defaultFilters,
+            [
+                'terminal' => $this->terminalId,
+                'date_from' => $dateFrom->format('d.m.Y, H:i'),
+                'date_to' => $dateTo->format('d.m.Y, H:i'),
+                'sale_total' => $saleTotal,
+                'sale_by_card' => $saleByCard,
+                'sale_by_cash' => $saleByCash,
+                'return_total' => -$returnTotal,
+                'return_by_card' => -$returnByCard,
+                'return_by_cash' => -$returnByCash,
+                'cash_total' => $saleByCash - $returnByCash,
+                'card_total' => $saleByCard - $returnByCard,
+                'overall_total' => $overallTotal,
+            ]
+        )->withCookie(cookie($this->rememberKey, $request->getToRemember()));
+    }
+
+    /**
+     * Get transactions list.
+     *
+     * @param ApiListRequest $request
+     *
+     * @return  JsonResponse
+     */
+    public function fiscal(ApiListRequest $request): JsonResponse
+    {
+        $gate = $request->input('gate');
+        $id = $request->input('id');
+
+        $name = $gate . '/' . $id . '.txt';
+
+        try {
+            $fiscal = Storage::disk('fiscal')->get($name);
+        } catch (FileNotFoundException $e) {
+            return APIResponse::error('Информация о чеке не найдена.');
+        }
+
+        return APIResponse::response([
+            'fiscal' => $fiscal,
+        ]);
+    }
+
+    public function export(ApiListRequest $request): JsonResponse
+    {
+        $this->defaultFilters['date_from'] = Carbon::now()->startOfDay()->format('Y-m-d\TH:i');
+        $this->defaultFilters['date_to'] = Carbon::now()->endOfDay()->format('Y-m-d\TH:i');
+        $filters = $request->filters($this->defaultFilters, $this->rememberFilters, $this->rememberKey);
+
+        $now = Carbon::now();
+
+        $query = $this->getListQuery($request, $filters, Currents::get($request));
+
+        $titles = [
+            'Дата и время',
+            '№ заказа',
+            'Тип',
+            'Чек',
+            'Сумма',
+            'Касса',
+            'Кассир'
+        ];
+        $payments=$query->get();
+        $payments->transform(function (Payment $payment) {
+            return [
+                'date' => $payment->created_at->format('d.m.Y H:i'),
+                'order_id' => $payment->order_id,
+                'status' => $payment->status->name,
+                'fiscal' => $payment->fiscal,
+                'total' => $payment->total,
+                'terminal' => $payment->terminal->name ?? null,
+                'position' => $payment->position ? $payment->position->user->profile->compactName : null,
+            ];
+        });
+
+        $spreadsheet = new Spreadsheet();
+
+        $spreadsheet->setActiveSheetIndex(0)->setTitle('Транзакции по кассам')->setShowRowColHeaders(true);
+
+        $spreadsheet->getActiveSheet()->fromArray($titles, '—', 'A1');
+        $spreadsheet->getActiveSheet()->fromArray($payments->toArray(), '—', 'A2');
+        foreach(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'] as $col) {
+            $spreadsheet->getActiveSheet()->getColumnDimension($col)->setAutoSize(true);
+        }
+
+        ob_start();
+        $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
+        $writer->save('php://output');
+        $export = ob_get_clean();
+
+        return APIResponse::response([
+            'file' => base64_encode($export),
+            'file_name' => 'Транзакции по кассам ' . $now->format('Y-m-d H:i'),
+            'type' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        ]);
+    }
+
+    protected function getListQuery(ApiListRequest $request, array $filters, Currents $current): Builder
+    {
+        $dateFrom = Carbon::parse($filters['date_from'])->seconds(0)->microseconds(0);
+        $dateTo = Carbon::parse($filters['date_to'])->seconds(59)->microseconds(999);
+
+        $query = Payment::query()
+            ->with(['status', 'order', 'terminal', 'position', 'position.user', 'position.user.profile']);
 
         if ($current->isStaffAdmin()) {
             if ($request->has('terminal_id') && $request->input('terminal_id') !== null) {
-                $query->where('terminal_id', $terminalId = $request->input('terminal_id'));
+                $query->where('terminal_id', $this->terminalId = $request->input('terminal_id'));
             } else if (!empty($filters['terminal_id'])) {
-                $query->where('terminal_id', $terminalId = $filters['terminal_id']);
+                $query->where('terminal_id', $this->terminalId = $filters['terminal_id']);
             }
         } else if ($current->isStaffTerminal()) {
-            $query->where('terminal_id', $terminalId = $current->terminalId());
+            $query->where('terminal_id', $this->terminalId = $current->terminalId());
         }
 
         if (!empty($filters['select'])) {
@@ -113,90 +263,8 @@ class TransactionsRegistryController extends ApiController
                 $query->where('created_at', '<=', $dateTo);
             }
         }
-
-        $queryBackup = $query->clone();
-        $payments = $query->paginate($request->perPage(10, $this->rememberKey));
-
-        /** @var LengthAwarePaginator $payments */
-        $payments->transform(function (Payment $payment) {
-            return [
-                'id' => $payment->id,
-                'gate' => $payment->gate,
-                'external_id' => $payment->external_id,
-                'date' => $payment->created_at->format('d.m.Y'),
-                'time' => $payment->created_at->format('H:i'),
-                'order_id' => $payment->order_id,
-                'order_external_id' => $payment->order->external_id ?? null,
-                'status' => $payment->status->name,
-                'status_id' => $payment->status_id,
-                'fiscal' => $payment->fiscal,
-                'total' => $payment->total,
-                'by_card' => $payment->by_card,
-                'by_cash' => $payment->by_cash,
-                'terminal' => $payment->terminal->name ?? null,
-                'terminal_id' => $payment->terminal_id,
-                'position' => $payment->position ? $payment->position->user->profile->compactName : null,
-                'position_id' => $payment->position->user_id ?? null,
-            ];
-        });
-
-        $saleQuery = $queryBackup->clone()->where('status_id', PaymentStatus::sale);
-        $returnQuery = $queryBackup->clone()->where('status_id', PaymentStatus::return);
-
-        $saleTotal = PriceConverter::storeToPrice($saleQuery->sum('total'));
-        $saleByCard = PriceConverter::storeToPrice($saleQuery->sum('by_card'));
-        $saleByCash = PriceConverter::storeToPrice($saleQuery->sum('by_cash'));
-
-        $returnTotal = PriceConverter::storeToPrice($returnQuery->sum('total'));
-        $returnByCard = PriceConverter::storeToPrice($returnQuery->sum('by_card'));
-        $returnByCash = PriceConverter::storeToPrice($returnQuery->sum('by_cash'));
-
-        $overallTotal = $saleTotal - $returnTotal;
-
-        return APIResponse::list(
-            $payments,
-            ['Дата и время', '№ заказа', 'Тип, чек', 'Сумма', 'Касса, кассир', 'Идентификаторы'],
-            $filters,
-            $this->defaultFilters,
-            [
-                'terminal' => $terminalId,
-                'date_from' => $dateFrom->format('d.m.Y, H:i'),
-                'date_to' => $dateTo->format('d.m.Y, H:i'),
-                'sale_total' => $saleTotal,
-                'sale_by_card' => $saleByCard,
-                'sale_by_cash' => $saleByCash,
-                'return_total' => -$returnTotal,
-                'return_by_card' => -$returnByCard,
-                'return_by_cash' => -$returnByCash,
-                'cash_total' => $saleByCash - $returnByCash,
-                'card_total' => $saleByCard - $returnByCard,
-                'overall_total' => $overallTotal,
-            ]
-        )->withCookie(cookie($this->rememberKey, $request->getToRemember()));
+        return $query;
     }
 
-    /**
-     * Get transactions list.
-     *
-     * @param ApiListRequest $request
-     *
-     * @return  JsonResponse
-     */
-    public function fiscal(ApiListRequest $request): JsonResponse
-    {
-        $gate = $request->input('gate');
-        $id = $request->input('id');
 
-        $name = $gate . '/' . $id . '.txt';
-
-        try {
-            $fiscal = Storage::disk('fiscal')->get($name);
-        } catch (FileNotFoundException $e) {
-            return APIResponse::error('Информация о чеке не найдена.');
-        }
-
-        return APIResponse::response([
-            'fiscal' => $fiscal,
-        ]);
-    }
 }
--- resources/js/Pages/Admin/Registries/Parts/AdminTransactionsRegistry.vue
+++ resources/js/Pages/Admin/Registries/Parts/AdminTransactionsRegistry.vue
@@ -50,6 +50,11 @@
                 <LayoutFiltersItem :title="'Поиск по номеру заказа, транзакции, сумме'">
                     <InputSearch v-model="list.search" @change="list.load()"/>
                 </LayoutFiltersItem>
+                <div style="display: flex; align-items: flex-end; margin-left: 10px;">
+                    <GuiActionsMenu :title="null">
+                        <span class="link" @click="excelExport">Экспорт в Excel</span>
+                    </GuiActionsMenu>
+                </div>
             </template>
         </LayoutFilters>
 
@@ -61,7 +66,8 @@
                         <div>{{ transaction['time'] }}</div>
                     </ListTableCell>
                     <ListTableCell>
-                        <RouterLink v-if="transaction['order_id']" class="link" :to="{name: 'order-info', params: {id: transaction['order_id']}}"
+                        <RouterLink v-if="transaction['order_id']" class="link"
+                                    :to="{name: 'order-info', params: {id: transaction['order_id']}}"
                                     v-html="highlight(transaction['order_id'], true)"/>
                         <span v-else>—</span>
                     </ListTableCell>
@@ -72,25 +78,30 @@
                     </ListTableCell>
                     <ListTableCell>
                         <div><b v-html="highlight(transaction['total'], true) + ' руб.'"></b></div>
-                        <div v-if="transaction['by_card']" v-html="'безнал.: ' + highlight(transaction['by_card'], true) + ' руб.'"></div>
-                        <div v-if="transaction['by_cash']" v-html="'нал.: ' + highlight(transaction['by_cash'], true) + ' руб.'"></div>
+                        <div v-if="transaction['by_card']"
+                             v-html="'безнал.: ' + highlight(transaction['by_card'], true) + ' руб.'"></div>
+                        <div v-if="transaction['by_cash']"
+                             v-html="'нал.: ' + highlight(transaction['by_cash'], true) + ' руб.'"></div>
                     </ListTableCell>
                     <ListTableCell>
                         <div>
-                            <RouterLink v-if="transaction['terminal_id']" class="link" :to="{name: 'terminals-view', params: {id: transaction['terminal_id']}}">
+                            <RouterLink v-if="transaction['terminal_id']" class="link"
+                                        :to="{name: 'terminals-view', params: {id: transaction['terminal_id']}}">
                                 {{ transaction['terminal'] }}
                             </RouterLink>
                             <span v-else>—</span>
                         </div>
                         <div>
-                            <RouterLink v-if="transaction['position_id']" class="link" :to="{name: 'staff-view', params: {id: transaction['position_id']}}">
+                            <RouterLink v-if="transaction['position_id']" class="link"
+                                        :to="{name: 'staff-view', params: {id: transaction['position_id']}}">
                                 {{ transaction['position'] }}
                             </RouterLink>
                             <span v-else>—</span>
                         </div>
                     </ListTableCell>
                     <ListTableCell>
-                        <div>{{ transaction['gate'] }}: <span v-html="highlight(transaction['external_id'])"></span></div>
+                        <div>{{ transaction['gate'] }}: <span v-html="highlight(transaction['external_id'])"></span>
+                        </div>
                         <div>ID заказа: <span v-html="highlight(transaction['order_external_id'])"></span></div>
                     </ListTableCell>
                 </ListTableRow>
@@ -102,17 +113,25 @@
         <Pagination :pagination="list.pagination" @pagination="(page, per_page) => list.load(page, per_page)"/>
 
         <GuiContainer mt-30>
-            <GuiHeading text-lg>Статистика за период: <span class="bold">{{ list.payload['date_from'] }} — {{ list.payload['date_to'] }}</span></GuiHeading>
-            <GuiHeading text-md mt-20 bold v-if="list.payload['terminal']">Касса №{{ list.payload['terminal'] }}</GuiHeading>
+            <GuiHeading text-lg>Статистика за период: <span class="bold">{{
+                    list.payload['date_from']
+                }} — {{ list.payload['date_to'] }}</span></GuiHeading>
+            <GuiHeading text-md mt-20 bold v-if="list.payload['terminal']">Касса №{{
+                    list.payload['terminal']
+                }}
+            </GuiHeading>
             <GuiHeading text-md mt-20 bold v-else>Касса: все</GuiHeading>
             <GuiContainer pr-40 pt-15 w-350px>
                 <GuiValue :class="'w-300px'" :title="'приход общ.'">{{ list.payload['sale_total'] }} руб.</GuiValue>
                 <GuiValue :class="'w-300px'" :title="'приход нал.'">{{ list.payload['sale_by_cash'] }} руб.</GuiValue>
-                <GuiValue :class="'w-300px'" :title="'приход безнал.'">{{ list.payload['sale_by_card'] }} руб.</GuiValue>
+                <GuiValue :class="'w-300px'" :title="'приход безнал.'">{{ list.payload['sale_by_card'] }} руб.
+                </GuiValue>
             </GuiContainer>
             <GuiContainer pr-40 pt-15 w-350px>
-                <GuiValue :class="'w-300px'" :title="'возврат нал.'">{{ list.payload['return_by_cash'] }} руб.</GuiValue>
-                <GuiValue :class="'w-300px'" :title="'возврат безнал.'">{{ list.payload['return_by_card'] }} руб.</GuiValue>
+                <GuiValue :class="'w-300px'" :title="'возврат нал.'">{{ list.payload['return_by_cash'] }} руб.
+                </GuiValue>
+                <GuiValue :class="'w-300px'" :title="'возврат безнал.'">{{ list.payload['return_by_card'] }} руб.
+                </GuiValue>
             </GuiContainer>
         </GuiContainer>
 
@@ -147,6 +166,7 @@ import InputDropDown from "@/Components/Inputs/InputDropDown";
 import GuiContainer from "@/Components/GUI/GuiContainer";
 import GuiHeading from "@/Components/GUI/GuiHeading";
 import GuiValue from "@/Components/GUI/GuiValue";
+import GuiActionsMenu from "@/Components/GUI/GuiActionsMenu.vue";
 
 export default {
     props: {
@@ -154,6 +174,7 @@ export default {
     },
 
     components: {
+        GuiActionsMenu,
         GuiValue,
         GuiHeading,
         GuiContainer,
@@ -220,6 +241,44 @@ export default {
         highlight(text, full = false) {
             return this.$highlight(String(text), String(this.list.search), full);
         },
+        excelExport() {
+            this.$dialog.show('Экспортировать ' + this.list.pagination.total + ' записей в Excel?',
+                null,
+                'blue',
+                [
+                    this.$dialog.button('yes', 'Экспортировать', 'blue'),
+                    this.$dialog.button('no', 'Отмена', 'default'),
+                ]
+            )
+                .then(result => {
+                    if (result === 'yes') {
+                        this.is_exporting = true;
+                        let options = {
+                            filters: this.list.filters,
+                            search: this.list.search,
+                        }
+                        axios.post('/api/registries/transactions/export', options)
+                            .then(response => {
+                                let file = atob(response.data.data['file']);
+                                let byteNumbers = new Array(file.length);
+                                for (let i = 0; i < file.length; i++) {
+                                    byteNumbers[i] = file.charCodeAt(i);
+                                }
+                                let byteArray = new Uint8Array(byteNumbers);
+                                let blob = new Blob([byteArray], {type: response.data.data['type']});
+
+                                saveAs(blob, response.data.data['file_name'], {autoBom: true});
+                            })
+                            .catch(error => {
+                                this.$toast.error(error.response.data['message']);
+                            })
+                            .finally(() => {
+                                this.is_exporting = false;
+                            });
+                    }
+                });
+        },
+
     },
 }
 </script>
