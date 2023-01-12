eca2eed1731ef24990be8727264ab901229ced60
lv <lozovoyv@gmail.com>
2023-01-12 16:56:00 +0300

    add sber callback route

--- routes/services.php
+++ routes/services.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('/services')->group(function () {
     require base_path('routes/services/lifepos.php');
     require base_path('routes/services/lifepay.php');
+    require base_path('routes/services/sber.php');
 });
