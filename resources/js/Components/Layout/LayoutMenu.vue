5573bd6613881218b36487228e00504e8dc0fefe
sm <dima.zherdev@gmail.com>
2023-02-16 12:09:06 +0300

    Mobile menu fix

--- resources/js/Components/Layout/LayoutMenu.vue
+++ resources/js/Components/Layout/LayoutMenu.vue
@@ -1,14 +1,16 @@
 <template>
-    <div class="application__menu-burger" @click="burgerToggle()">
-        <span></span>
-    </div>
+    <div class="application__menu-wrapper" :class="{'application__menu-wrapper-active': opened}">
+        <div class="application__menu-burger" @click="burgerToggle()">
+            <span></span>
+        </div>
 
-    <nav class="application__menu">
-        <layout-menu-item v-for="(item, key) in menu"
-                          :key="key"
-                          :item="item"
-        />
-    </nav>
+        <nav class="application__menu">
+            <layout-menu-item v-for="(item, key) in menu"
+                              :key="key"
+                              :item="item"
+            />
+        </nav>
+    </div>
 </template>
 
 <script>
@@ -23,15 +25,88 @@ export default {
         LayoutMenuItem,
     },
 
+    data: () => ({
+        opened: false,
+    }),
+
     methods: {
         burgerToggle() {
-            let burgerMenu = document.querySelector('.application__header-menu');
-            if ( burgerMenu.classList.contains('active') ) {
-                burgerMenu.classList.remove('active')
-            } else {
-                burgerMenu.classList.add('active')
-            }
+            this.opened = !this.opened
         }
     }
 }
 </script>
+
+<style lang="scss">
+    @media (max-width: 767px) {
+        .application__menu-wrapper {
+            .application__menu {
+                flex-direction: column;
+                height: auto;
+                row-gap: 20px;
+                padding-top: 20px;
+                display: none;
+
+                &-submenu-0 {
+                    bottom: -5px;
+                }
+
+                &-burger {
+                    transition: all 0.3s ease;
+
+                    span {
+                        width: 20px;
+                        height: 2px;
+                        display: inline-block;
+                        background: #0B68C2;
+                        position: relative;
+
+                        &::before {
+                            position: absolute;
+                            content: '';
+                            top: -7px;
+                            left: 0;
+                            width: 20px;
+                            height: 2px;
+                            display: inline-block;
+                            background: #0B68C2;
+                        }
+
+                        &::after {
+                            position: absolute;
+                            content: '';
+                            bottom: -7px;
+                            left: 0;
+                            width: 20px;
+                            height: 2px;
+                            display: inline-block;
+                            background: #0B68C2;
+                        }
+                    }
+                }
+            }
+        }
+
+        .application__menu-wrapper-active {
+
+            .application__menu-burger {
+                span {
+                    transform: rotate(135deg);
+
+                    &::before {
+                        transform: rotate(90deg);
+                        top: 0;
+                    }
+
+                    &::after {
+                        display: none;
+                    }
+                }
+            }
+
+            .application__menu {
+                display: flex;
+            }
+        }
+    }
+</style>
