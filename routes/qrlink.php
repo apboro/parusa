8cfe34f41ba64ebbe4bd079e62e0fa3ab26e3cc0
lv <lozovoyv@gmail.com>
2023-03-28 18:16:27 +0300

    update cors

--- routes/qrlink.php
+++ routes/qrlink.php
@@ -3,5 +3,5 @@
 use App\Http\Controllers\QrCode\QrCodeRedirectController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/qrlink/{hash}', [QrCodeRedirectController::class, 'redirect'])->name('qrlink');
+Route::get('/qrlink/{hash}', [QrCodeRedirectController::class, 'redirect'])->name('qrlink')->middleware(['checkout']);
 
