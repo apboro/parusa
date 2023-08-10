16da4e58785c63a290d96a30558849b68087fea2
apboro <borodachev@gmail.com>
2023-08-10 15:55:53 +0300

    order by trips.start_at

--- app/Http/Controllers/API/Trips/TripsListController.php
+++ app/Http/Controllers/API/Trips/TripsListController.php
@@ -58,7 +58,8 @@ class TripsListController extends ApiController
             }])
             ->join('excursions','excursions.id','=', 'trips.excursion_id')
             ->groupByRaw("case when `is_single_ticket`=1 THEN excursion_id else trips.id end")
-            ->orderBy('is_single_ticket', 'desc');
+            ->orderBy('is_single_ticket', 'desc')
+            ->orderBy('trips.start_at');
 
         // apply filters
         if (!empty($filters = $request->filters($this->defaultFilters, $this->rememberFilters, $this->rememberKey))) {
