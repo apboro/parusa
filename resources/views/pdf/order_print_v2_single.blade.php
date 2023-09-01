107ae3321a17083ccf22053a5c8a8b3810bf8934
apboro <borodachev@gmail.com>
2023-09-01 10:51:15 +0300

    fix order_print

--- resources/views/pdf/order_print_v2_single.blade.php
+++ resources/views/pdf/order_print_v2_single.blade.php
@@ -64,7 +64,7 @@ use App\Models\Tickets\Ticket;
                     <div style="position:absolute; left: 95pt; top: 105pt; width: 100%; font-size: 7pt; text-align: center;">Стоимость</div>
 
                     <div style="position:absolute; left: 90pt; top: 115pt; width: 100%;">
-                        <div style="font-size: 8pt;text-align: center">N заказа {{ $ticket->order->neva_travel_order_number ?? $ticket->order_id }}, N билета {{ $ticket->id }}</div>
+                        <div style="font-size: 8pt;text-align: center">N заказа {{ $ticket->order->additionalData?->provider_order_id ?? $ticket->order_id }}, N билета {{ $ticket->id }}</div>
                     </div>
                 </div>
             </div>
