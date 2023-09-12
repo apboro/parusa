b7ea0e2dd4abc714980e38712b0157bbbddae4bf
apboro <borodachev@gmail.com>
2023-09-12 14:23:08 +0300

    partner filter fix

--- app/Http/Controllers/API/Trips/TripsSelectListController.php
+++ app/Http/Controllers/API/Trips/TripsSelectListController.php
@@ -183,6 +183,7 @@ class TripsSelectListController extends ApiController
                         $trip->whereDate('start_at', Carbon::parse($filters['date']));
                     })
                     ->where('type_id', $filters['excursion_type_id'])
+                    ->whereNotNull('excursions.type_id')
                     ->where('status_id', 1)
                     ->get(['excursions.id', 'excursions.name']),
                 'piers_filter' => Pier::query()
