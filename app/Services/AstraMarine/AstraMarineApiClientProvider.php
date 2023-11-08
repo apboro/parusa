2248a285ee58a3ca61ce48865e8180b23c926a1f
apboro <borodachev@gmail.com>
2023-11-08 14:19:01 +0300

    astra-marine api client

---
+++ app/Services/AstraMarine/AstraMarineApiClientProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Services\AstraMarine;
+
+use Illuminate\Support\Facades\Http;
+
+class AstraMarineApiClientProvider
+{
+    private string $baseUrl;
+
+    public function __construct()
+    {
+        $this->baseUrl = config('astra-marine.api_url');
+    }
+
+    public function get(string $uri, array $query = []): array
+    {
+        $response = Http::get($this->baseUrl . $uri, $query);
+
+        return [
+            'status' => $response->status(),
+            'headers' => $response->headers(),
+            'body' => $response->json(),
+        ];
+    }
+
+    public function post(string $uri, array $data = []): array
+    {
+        $data['email'] = 'info@parus-a.ru';
+        $response = Http::post($this->baseUrl . $uri, $data);
+
+        return [
+            'status' => $response->status(),
+            'headers' => $response->headers(),
+            'body' => $response->json(),
+        ];
+    }
+}
---
+++ config/astra-marine.php
@@ -0,0 +1,4 @@
+<?php
+return [
+    "api_url" => env('ASTRA_MARINE_API_URL', 'http://api.astra-marine.ru:8088/InternetTestJson/hs/InternetSale/V1/'),
+];
