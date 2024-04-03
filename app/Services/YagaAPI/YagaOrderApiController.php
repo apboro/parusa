0d1b63687ce35ee405c9f012fda11524b037cb3c
apboro <borodachev@gmail.com>
2024-04-03 14:19:09 +0300

    yaga api v0.7 filters seats

--- app/Services/YagaAPI/YagaOrderApiController.php
+++ app/Services/YagaAPI/YagaOrderApiController.php
@@ -6,6 +6,7 @@ use App\Models\Sails\Trip;
 use App\Services\YagaAPI\Model\AvailableSeats;
 use App\Services\YagaAPI\Requests\Order\AvailableSeatsRequest;
 use App\Services\YagaAPI\Requests\Order\ReserveTicketsRequest;
+use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
 use OpenApi\Attributes as OA;
 
@@ -13,7 +14,31 @@ class YagaOrderApiController
 {
     public function availableSeats(AvailableSeatsRequest $request): JsonResponse
     {
-        $trip = Trip::with(['excursion', 'excursion.ratesLists', 'tickets'])->where('id', ($request->input('sessionId')))->first();
+        $data = $request->all();
+
+        if (!$data['sessionId']) {
+            return response()->json();
+        }
+
+        $trip = Trip::with(['excursion', 'excursion.ratesLists', 'tickets'])->where('id', $data['sessionId'])->first();
+
+        if (!$trip) {
+            return response()->json();
+        }
+
+        if (!empty($data['venueId']) && $trip->ship->id != $data['venueId']) {
+            return response()->json();
+        }
+        if (!empty($data['eventId']) && $trip->excursion->id != $data['eventId']) {
+            return response()->json();
+        }
+        if (!empty($data['hallId']) && $trip->ship->id != $data['hallId']) {
+            return response()->json();
+        }
+        if (!empty($data['sessionTime']) && $trip->start_at->timestamp != $data['sessionTime']){
+            return response()->json();
+        }
+
         $results = AvailableSeats::getResource($trip);
 
         return response()->json([$results]);
