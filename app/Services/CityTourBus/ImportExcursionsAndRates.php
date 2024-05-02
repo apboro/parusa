fdc5ec728a391cee834d528856d67efaaae6ab52
apboro <borodachev@gmail.com>
2024-05-02 11:28:29 +0300

    city tour import refactor

--- app/Services/CityTourBus/ImportExcursionsAndRates.php
+++ app/Services/CityTourBus/ImportExcursionsAndRates.php
@@ -12,85 +12,91 @@ use App\Models\Ships\Ship;
 use App\Models\Tickets\TicketRate;
 use App\Models\Tickets\TicketsRatesList;
 use Carbon\Carbon;
+use Exception;
+use Illuminate\Support\Facades\Log;
 
 
 class ImportExcursionsAndRates
 {
     public function run()
     {
-        $cityTourRepo = new CityTourRepository();
 
-        $cityTourExcursions = $cityTourRepo->getExcursions()['body'];
+        try {
+            $cityTourRepo = new CityTourRepository();
+            $cityTourExcursions = $cityTourRepo->getExcursions()['body'];
+            $parusaExcursions = Excursion::where('provider_id', Provider::city_tour)->get();
+            foreach ($cityTourExcursions as $externalExcursion) {
+                $foundExcursion = $parusaExcursions->first(function (Excursion $exc) use ($externalExcursion) {
+                    return $exc->additionalData->provider_excursion_id == $externalExcursion['id'];
+                });
+                if (!$foundExcursion) {
+                    $newExcursion = new Excursion();
+                    $newExcursion->name = $externalExcursion['short_title'];
+                    $newExcursion->status_id = 2;
+                    $newExcursion->provider_id = Provider::city_tour;
+                    $newExcursion->save();
+                    $info = $newExcursion->info;
+                    $info->description = strip_tags($externalExcursion['description']);
+                    $info->save();
 
-        $parusaExcursions = Excursion::where('provider_id', Provider::city_tour)->get();
+                    $additionalData = new AdditionalDataExcursion();
+                    $additionalData->excursion_id = $newExcursion->id;
+                    $additionalData->provider_excursion_id = $externalExcursion['id'];
+                    $additionalData->provider_id = Provider::city_tour;
+                    $additionalData->save();
+                }
 
-        foreach ($cityTourExcursions as $externalExcursion) {
-            $foundExcursion = $parusaExcursions->first(function (Excursion $exc) use ($externalExcursion) {
-                return $exc->additionalData->provider_excursion_id == $externalExcursion['id'];
-            });
-            if (!$foundExcursion) {
-                $newExcursion = new Excursion();
-                $newExcursion->name = $externalExcursion['short_title'];
-                $newExcursion->status_id = 2;
-                $newExcursion->provider_id = Provider::city_tour;
-                $newExcursion->save();
-                $info = $newExcursion->info;
-                $info->description = strip_tags($externalExcursion['description']);
-                $info->save();
+                $this->addOrUpdateGradesInDictionary($externalExcursion['tickets_categories']);
 
-                $additionalData = new AdditionalDataExcursion();
-                $additionalData->excursion_id = $newExcursion->id;
-                $additionalData->provider_excursion_id = $externalExcursion['id'];
-                $additionalData->provider_id = Provider::city_tour;
-                $additionalData->save();
-            }
+                $grades = $externalExcursion['tickets_categories'];
 
-            $this->addOrUpdateGradesInDictionary($externalExcursion['tickets_categories']);
+                $ratesList = $this->createOrUpdateRateList($foundExcursion ?? $newExcursion, $grades);
 
-            $grades = $externalExcursion['tickets_categories'];
+                if (!$ratesList) {
+                    continue;
+                }
 
-            $ratesList = $this->createOrUpdateRateList($foundExcursion ?? $newExcursion, $grades);
-
-            foreach ($grades as $grade) {
-                if ($grade['date_to'] === null || ($grade['date_to'] && Carbon::parse($grade['date_to']) > Carbon::now())) {
-                    TicketRate::updateOrCreate(
-                        [
-                            'rate_id' => $ratesList->id,
-                            'grade_id' => $grade['id'],
-                        ],
-                        [
-                            'base_price' => $grade['price'],
-                            'min_price' => $grade['price'],
-                            'max_price' => $grade['price'],
-                            'commission_type' => 'fixed',
-                            'commission_value' => 100,
-                            'site_price' => $grade['price'],
-                        ]
-                    );
+                foreach ($grades as $grade) {
+                    if ($grade['date_to'] === null || ($grade['date_to'] && Carbon::parse($grade['date_to']) > Carbon::now())) {
+                        TicketRate::updateOrCreate(
+                            [
+                                'rate_id' => $ratesList->id,
+                                'grade_id' => $grade['id'],
+                            ],
+                            [
+                                'base_price' => $grade['price'],
+                                'min_price' => $grade['price'],
+                                'max_price' => $grade['price'],
+                                'commission_type' => 'fixed',
+                                'commission_value' => 100,
+                                'site_price' => $grade['price'],
+                            ]
+                        );
+                    }
                 }
             }
-        }
-
-        $pier = Pier::firstOrCreate([
-            'external_id' => 54,
-            'name' => 'Остановка автобусов City Tour',
-            'provider_id' => Provider::city_tour,
+            $pier = Pier::firstOrCreate([
+                'external_id' => 54,
+                'name' => 'Остановка автобусов City Tour',
+                'provider_id' => Provider::city_tour,
             ]);
-        $info = $pier->info;
-        $info->address = 'Санкт-Петербург, Площадь Островского, дом 1/3';
-        $info->phone = '+7 (812) 648-12-28';
-        $info->save();
-
-        Ship::firstOrCreate([
-            'name' => 'Автобус City Tour',
-            'enabled' => 1,
-            'order' => 100,
-            'status_id' => 1,
-            'owner' => 'CityTour',
-            'capacity' => 40,
-            'label' => 'CTB',
-            'provider_id' => Provider::city_tour
-        ]);
+            $info = $pier->info;
+            $info->address = 'Санкт-Петербург, Площадь Островского, дом 1/3';
+            $info->phone = '+7 (812) 648-12-28';
+            $info->save();
+            Ship::firstOrCreate([
+                'name' => 'Автобус City Tour',
+                'enabled' => 1,
+                'order' => 100,
+                'status_id' => 1,
+                'owner' => 'CityTour',
+                'capacity' => 40,
+                'label' => 'CTB',
+                'provider_id' => Provider::city_tour
+            ]);
+        } catch (Exception $e) {
+            Log::channel('city_tour')->error('import error: ' .$e->getMessage() . ' ' . $e->getFile(). ' ' . $e->getLine());
+        }
 
     }
 
@@ -99,18 +105,18 @@ class ImportExcursionsAndRates
     {
         foreach ($grades as $grade) {
             if ($grade['date_to'] === null || ($grade['date_to'] && Carbon::parse($grade['date_to']) > Carbon::now())) {
-                TicketGrade::updateOrCreate(['id' => $grade['id'], 'name' => $grade['category_name']],
+                TicketGrade::updateOrCreate(['id' => $grade['id'], 'provider_id' => Provider::city_tour],
                     [
+                        'name' => $grade['category_name'],
                         'enabled' => 1,
                         'locked' => 1,
                         'external_grade_name' => $grade['category_name'],
-                        'provider_id' => Provider::city_tour,
                     ]);
             }
         }
     }
 
-    public function createOrUpdateRateList($innerExcursion, $externalExcursionRates): TicketsRatesList
+    public function createOrUpdateRateList($innerExcursion, $externalExcursionRates): ?TicketsRatesList
     {
         foreach ($externalExcursionRates as $rate) {
             if ((!$rate['date_from'] && !$rate['date_to'])
@@ -133,7 +139,7 @@ class ImportExcursionsAndRates
             }
     }
 
-        return $ticketsRatesList;
+        return $ticketsRatesList ?? null;
     }
 
 }
