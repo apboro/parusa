f611b38afc7ada275acb37249696dd2d7d4f48e1
apboro <borodachev@gmail.com>
2024-06-10 15:03:10 +0300

    db transaction in terminal make order

--- app/Http/Controllers/API/Order/TerminalMakeOrderController.php
+++ app/Http/Controllers/API/Order/TerminalMakeOrderController.php
@@ -117,7 +117,7 @@ class TerminalMakeOrderController extends ApiEditController
 
         try {
 
-//            DB::transaction(static function () use (&$order, $tickets, $current, $data, $partnerId) {
+            DB::transaction(static function () use (&$order, $tickets, $current, $data, $partnerId) {
                 // create order
 
                 $order = (new CreateOrderFromTerminal($current))->execute($tickets, $data, $partnerId);
@@ -139,10 +139,10 @@ class TerminalMakeOrderController extends ApiEditController
                 }
 
                 $order->setStatus(OrderStatus::terminal_wait_for_pay);
-//            });
+            });
         } catch (Exception $exception) {
             Log::error('terminal make order error: '. $exception->getMessage().' '. $exception->getFile(). ' '. $exception->getLine());
-            return APIResponse::error($exception->getMessage().' '. $exception->getFile(). ' '. $exception->getLine());
+            return APIResponse::error('Не удалось оформить заказ. '. $exception->getMessage());
         }
 
         return APIResponse::success($successMessage, ['order_id' => $order->id]);
