ae6195ebbf7f19fda0c9adfe253ec8be161e2032
apboro <borodachev@gmail.com>
2024-07-19 15:03:05 +0300

    fix refresh astra

--- app/Console/Commands/AstraMarineRefreshCommand.php
+++ app/Console/Commands/AstraMarineRefreshCommand.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use App\Models\Dictionaries\Provider;
+use App\Models\Dictionaries\TripSaleStatus;
+use App\Models\Dictionaries\TripStatus;
 use App\Models\Sails\Trip;
 use App\Services\AstraMarine\AstraMarineRepository;
 use Illuminate\Console\Command;
@@ -21,13 +23,24 @@ class AstraMarineRefreshCommand extends Command
             ->where('end_at', '<', now()->addDay())
             ->get();
 
-        foreach ($trips as $trip) {
+        Trip::query()->with(['additionalData'])
+            ->where('provider_id', Provider::astra_marine)
+            ->where('start_at', '>', now())
+            ->where('end_at', '<', now()->addDay())
+            ->update([
+                'status_id' => TripStatus::cancelled,
+                'sale_status_id' => TripSaleStatus::closed_automatically
+            ]);
 
+        foreach ($trips as $trip) {
             $response = (new AstraMarineRepository())->getEvents(['eventId' => $trip->additionalData->provider_trip_id])['body'];
             if (!empty($response['events'])) {
                 $trip->tickets_total = $response['events'][0]['availableSeats'];
+                $trip->sale_status_id = TripSaleStatus::selling;
+                $trip->status_id = TripStatus::regular;
                 $trip->save();
             }
+
         }
     }
 }
