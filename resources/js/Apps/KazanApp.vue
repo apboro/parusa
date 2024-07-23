f0191f92c8cc6fcfc9aaf4cc3222b70cd16c6821
apboro <borodachev@gmail.com>
2024-07-23 10:49:33 +0300

    kazan

--- resources/js/Apps/KazanApp.vue
+++ resources/js/Apps/KazanApp.vue
@@ -1,7 +1,13 @@
 <template>
-    kazan
+    <ShowcaseApp3 crm_url="http://localhost:8000"/>
 </template>
 
 <script>
 
+import {defineComponent} from "vue";
+import ShowcaseApp3 from "@/Apps/ShowcaseApp3.vue";
+
+export default defineComponent({
+    components: {ShowcaseApp3}
+})
 </script>
